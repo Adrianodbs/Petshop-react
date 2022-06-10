27871d2b1fcdf639e756b39596e96d4c15e59943
Adriano <adriano_alves_2008@hotmail.com>
2022-06-09 22:01:35 -0300

    pagina de erro

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './assets/css/base/base.css'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Pagina404 from './paginas/Pagina404'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sobre' element={<Sobre />} />
+      <Route path="*" element={<Pagina404 />} />
     </Routes>
   </Router>
   )
