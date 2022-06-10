e8e08b2dd30e0945a1406c2aba88d6ef0b3c9e3e
Adriano <adriano_alves_2008@hotmail.com>
2022-06-09 22:21:57 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
+
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sobre' element={<Sobre />} />
